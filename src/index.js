11e59f4874c34bff565c4db1c1fb883f8ddc60e4
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-30 22:33:59 +0300

    pre

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'modern-normalize';
+// import 'modern-normalize';
 // import { ThemeProvider } from 'styled-components';
 import { App } from 'components';
 // import './index.css';
